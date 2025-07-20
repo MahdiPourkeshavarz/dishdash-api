bedb9b1c4460271894ca075b9282b4e65b406967
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-07-20 10:05:34 +0330

    feat(database): using type orm and mongodb for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,9 +8,31 @@ import { PostsModule } from './posts/posts.module';
 import { UploadsModule } from './uploads/uploads.module';
 import { InteractionsModule } from './interactions/interactions.module';
 import { PlacesModule } from './places/places.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, UploadsModule, InteractionsModule, PlacesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get('DATABASE_URL'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    UploadsModule,
+    InteractionsModule,
+    PlacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
