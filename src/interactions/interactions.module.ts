e72015e467121741678e79549d9923464b057320
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-07-20 15:14:23 +0330

    feat(interaction): implemented logic for wishlist

--- src/interactions/interactions.module.ts
+++ src/interactions/interactions.module.ts
@@ -1,5 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Delete,
+  Get,
+  Module,
+  Param,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { InteractionsController } from './interactions.controller';
 import { InteractionsService } from './interactions.service';
 
@@ -7,4 +17,24 @@ import { InteractionsService } from './interactions.service';
   controllers: [InteractionsController],
   providers: [InteractionsService],
 })
-export class InteractionsModule {}
+export class InteractionsModule {
+  constructor(private readonly interactionsService: InteractionsService) {}
+
+  @Post('wishlist')
+  addToWishlist(@Request() req, @Body('placeId') placeId: string) {
+    const userId = req.user.sub; // Get userId from the validated JWT
+    return this.interactionsService.addToWishlist(userId, placeId);
+  }
+
+  @Delete('wishlist/:placeId')
+  removeFromWishlist(@Request() req, @Param('placeId') placeId: string) {
+    const userId = req.user.sub;
+    return this.interactionsService.removeFromWishlist(userId, placeId);
+  }
+
+  @Get('wishlist')
+  getWishlist(@Request() req) {
+    const userId = req.user.sub;
+    return this.interactionsService.getWishlistForUser(userId);
+  }
+}
